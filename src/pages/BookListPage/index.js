df557f52f873d49fba4b6d797a7c3b7cc51d2821
Glauber Neves <glaubergneves@gmail.com>
2024-03-18 14:21:01 -0300

    implementa filtro na tela de lista de livros

--- src/pages/BookListPage/index.js
+++ src/pages/BookListPage/index.js
@@ -1,28 +1,90 @@
-import React, { useState, useEffect } from 'react';
-import BookList from '../../components/BookList';
+import React, { useState, useEffect } from "react";
+import BookList from "../../components/BookList";
+import CampoTexto from "../../components/CampoTexto";
 
 const BookListPage = () => {
   const [books, setBooks] = useState([]);
+  const [filtroTitulo, setFiltroTitulo] = useState("");
+  const [filtroAutor, setFiltroAutor] = useState("");
+  const [filtroCategoria, setFiltroCategoria] = useState("");
+  const [livrosFiltrados, setFilteredBooks] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://t3t4-dfe-pb-grl-m1-default-rtdb.firebaseio.com/books.json');
+        const response = await fetch(
+          "https://t3t4-dfe-pb-grl-m1-default-rtdb.firebaseio.com/books.json"
+        );
         const data = await response.json();
         const booksArray = Object.values(data);
         setBooks(booksArray);
       } catch (error) {
-        console.error('Erro ao obter dados da API:', error);
+        console.error("Erro ao obter dados da API:", error);
       }
     };
 
     fetchData();
   }, []);
 
+  const applyFilters = () => {
+    const filtered = books.filter((book) => {
+      if (
+        filtroTitulo &&
+        !book.title.toLowerCase().includes(filtroTitulo.toLowerCase())
+      ) {
+        return false;
+      }
+      if (
+        filtroAutor &&
+        !book.author.toLowerCase().includes(filtroAutor.toLowerCase())
+      ) {
+        return false;
+      }
+      if (
+        filtroCategoria &&
+        !book.genre.toLowerCase().includes(filtroCategoria.toLowerCase())
+      ) {
+        return false;
+      }
+      return true;
+    });
+    setFilteredBooks(filtered);
+  };
+
+  React.useEffect(() => {
+    applyFilters();
+  }, [filtroTitulo, filtroAutor, filtroCategoria]);
+
   return (
     <div>
+      <CampoTexto
+        value={filtroTitulo}
+        placeholder="Filtrar por título"
+        obrigatorio={false}
+        aoAlterado={(valor) => setFiltroTitulo(valor)}
+      ></CampoTexto>
+      <CampoTexto
+        value={filtroAutor}
+        placeholder="Filtrar por autor"
+        obrigatorio={false}
+        aoAlterado={(valor) => setFiltroAutor(valor)}
+      ></CampoTexto>
+      <CampoTexto
+        value={filtroCategoria}
+        placeholder="Filtrar por categoria"
+        obrigatorio={false}
+        aoAlterado={(valor) => setFiltroCategoria(valor)}
+      ></CampoTexto>
+
       <h1>Lista de Livros Cadastrados</h1>
-      <BookList books={books} />
+      {livrosFiltrados.length === 0 ? (
+        <div>
+          <h3>Sem livros com esses filtros...</h3>
+          <BookList books={books} />
+        </div>
+      ) : (
+        <BookList books={livrosFiltrados} />
+      )}
     </div>
   );
 };
